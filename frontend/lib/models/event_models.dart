610134eecc911a2c536cc98f7689790196285484
qahramantaha <qahraman1999@gmail.com>
2026-04-10 22:32:18 +0100

    feat: add event filters for going, created by me, and private events

--- frontend/lib/models/event_models.dart
+++ frontend/lib/models/event_models.dart
@@ -29,6 +29,8 @@ class EventModel {
   final String category;
   final int goingCount;
   final bool isGoing;
+  final bool isPrivate;
+  final String createdBy;
   final List<AttendeeModel> attendees;
 
   EventModel({
@@ -42,6 +44,8 @@ class EventModel {
     required this.category,
     required this.goingCount,
     required this.isGoing,
+    required this.isPrivate,
+    required this.createdBy,
     required this.attendees,
   });
 
@@ -59,6 +63,8 @@ class EventModel {
       category: json['category'] ?? '',
       goingCount: json['goingCount'] ?? 0,
       isGoing: json['isGoing'] ?? false,
+      isPrivate: json['isPrivate'] ?? false,
+      createdBy: json['createdBy']?.toString() ?? '',
       attendees: attendeeList
           .map((attendee) => AttendeeModel.fromJson(attendee))
           .toList(),
